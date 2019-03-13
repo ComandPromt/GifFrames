67346f2c208d01c6ca8b9bd1eed9a76b28c16238
ComandPromt <vayase_a_ferrol@hotmail.com>
2019-03-13 12:58:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,15 @@
 <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
 <title>Subir archivos al servidor</title>
 <style type="text/css" media="screen">
+
 *{font-size:1em;
 transform: scale(1.1);
 }
+
 input{
  width: 200px;
 }
+
 </style>
 </head>
 <body>
@@ -20,68 +23,65 @@ input{
 <input type='submit' name="enviar" value='Subir archivo'>
 </form>
 </div>
-<?php 
-		include('funciones.php');
+
+<?php
+
+include 'funciones.php';
 comprobar_carpetas();
 $target_path = "uploads/";
-$avatar=$_FILES['uploadedfile']['name'];
+$avatar = $_FILES['uploadedfile']['name'];
 $nombre = substr($avatar, 0, -4);
-	$extension= substr($avatar, -4);
-	if(strlen($nombre)>15){
-		$nombre=substr($avatar, 0, 15);
-		$avatar=$nombre.$extension; 
-	}
+$extension = substr($avatar, -4);
 
-$imagen=basename($avatar);
-$longitud=strlen($imagen);
-$target_path .=$imagen;
+if (strlen($nombre) > 15) {
+    $nombre = substr($avatar, 0, 15);
+    $avatar = $nombre . $extension;
+}
 
-if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)){
+$imagen = basename($avatar);
+$longitud = strlen($imagen);
+$target_path .= $imagen;
 
+if (move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
 
-	if(substr($imagen,$longitud-3,$longitud)!="gif"){
-		unlink($target_path);
-	}
-	else{
-$numero=1;
-		chmod("uploads/", 0777);
-		rename($target_path ,'output/picture.gif');
-		chmod("output/picture.gif", 0777);
-		rmdir('uploads');
-		$numero=sacarframes();
-		if($numero>1){
-			date_default_timezone_set ('Europe/Madrid');
-		$nombre_zip=crear_zip();
-		
-		
-		for($x=1;$x<=$numero;$x++){
-		if($x<10){
-			unlink('output/frames/frame0'.$x.'.png');
-		}
-		else{
-			unlink('output/frames/frame'.$x.'.png');
-		}
-	}
-	unlink("output/picture.gif");
-		
-		
-		}
-		else{
-			if (file_exists("output/frames/frame01.png")) {
-        mkdir("output/frames/frame01.png", 0777, true);
+    if (substr($imagen, $longitud - 3, $longitud) != "gif") {
+        unlink($target_path);
+    } else {
+        $numero = 1;
+        chmod("uploads/", 0777);
+        rename($target_path, 'output/picture.gif');
+        chmod("output/picture.gif", 0777);
+        rmdir('uploads');
+        $numero = sacarframes();
+
+        if ($numero > 1) {
+
+            date_default_timezone_set('Europe/Madrid');
+            $nombre_zip = crear_zip();
+
+            for ($x = 1; $x <= $numero; $x++) {
+                if ($x < 10) {
+                    unlink('output/frames/frame0' . $x . '.png');
+                } else {
+                    unlink('output/frames/frame' . $x . '.png');
+                }
+            }
+            unlink("output/picture.gif");
+
+        } else {
+            if (file_exists("output/frames/frame01.png")) {
+                unlink("output/frames/frame01.png");
+            }
+
+        }
     }
-	
-	
-			
-	}
-	}
 
-	rmdir('output/frames');
-	rmdir('output');
-	header("Content-disposition: attachment; filename=$nombre_zip");
-	header("Content-type: MIME");
-	readfile($nombre_zip);
-	unlink($nombre_zip);
+    rmdir('output/frames');
+    rmdir('output');
+    header("Content-disposition: attachment; filename=$nombre_zip");
+    header("Content-type: MIME");
+    readfile($nombre_zip);
+    unlink($nombre_zip);
 }
 
 ?>
